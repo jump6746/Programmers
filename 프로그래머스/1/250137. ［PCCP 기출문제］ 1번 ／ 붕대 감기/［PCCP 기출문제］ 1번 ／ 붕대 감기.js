dd22af258988f0098e4f44ae9f8ada308ccafdc4
Jongmyeong Kim <102586637+jump6746@users.noreply.github.com>
2024-03-24 19:33:39 +0900

    Update ［PCCP 기출문제］ 1번 ／ 붕대 감기.js

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.js
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.js
@@ -1,17 +1,3 @@
-/* 
-  bandage = [시전 시간, 초당 회복량, 추가 회복량]
-  health  = 최대 체력
-  attack  = [공격 시간, 피해량]
-*/
-
-let bandage = [5, 1, 5];
-let health = 30;
-let attacks = [
-  [2, 10],
-  [9, 15],
-  [10, 5],
-  [11, 5],
-];
 
 function checkHealth(currentHealth, maxHealth) {
   if (currentHealth >= maxHealth) currentHealth = maxHealth;
@@ -54,25 +40,5 @@ function solution(bandage, health, attacks) {
   return currentHealth;
 }
 
-console.log(solution(bandage, health, attacks));
-
-/* 
-
-function solution(bandage, health, attacks) {
-  let currHealth = health;
-  let currTime = 0;
-
-  for (let [attackTime, damage] of attacks) {
-    let diffTime = attackTime - currTime - 1;
-    currHealth += diffTime * bandage[1] + Math.floor(diffTime / bandage[0]) * bandage[2];
-
-    if (currHealth > health) currHealth = health;
-    currHealth -= damage;
-    if (currHealth <= 0) return -1;
-    currTime = attackTime;
-  }
-
-  return currHealth;
-}
 
 */
